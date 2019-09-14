ce6c8040de39e8312f22cf22f5ce23534684fd0d
Alexander Shieh <teweishieh@gmail.com>
2019-09-14 14:26:02 +0800

    first

--- app.py
+++ app.py
@@ -602,7 +602,7 @@ def handle_message(event):
     #     )
     # )
 
-    line_bot_api.reply_message(event.reply_token, event.message.text)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 
 
