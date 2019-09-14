023bfd8724be7f9e8f45c5254ee5d52d0f954b25
voidism <b05901033@ntu.edu.tw>
2019-09-14 05:45:00 +0000

    fix

--- app.py
+++ app.py
@@ -42,10 +42,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print("Good")
-    line_bot_api.reply_message(reply_token, TextSendMessage(text='Hello World!'))
-    #line_bot_api.reply_message(
-    #    event.reply_token,
-    #    TextSendMessage(text=event.message.text))
+    # line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Hello World!'))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
