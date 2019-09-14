469589895409f73f4f71cc3a4f18272fbbd9f4e4
voidism <b05901033@ntu.edu.tw>
2019-09-14 06:10:18 +0000

    rm print

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    print(body)
+    #print(body)
 
     # handle webhook body
     try:
@@ -41,7 +41,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("Good")
+    #print("Good")
     # line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Hello World!'))
     line_bot_api.reply_message(
         event.reply_token,
